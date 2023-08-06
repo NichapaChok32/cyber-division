3cdaea499d9a73d452a9edf3e45eb657ce0f107a
Nichapa Chokaree <nichapachok32@gmail.com>
2023-08-06 16:26:32 +0700

    add responsive mobile and teams

--- components/Footers.vue
+++ components/Footers.vue
@@ -1,8 +1,11 @@
 <template>
   <div id="Footers">
     <div class="divider"></div>
-    <footer class="footers-main columns-3 grid">
-      <div class="footers-img">
+    <div class="footers-img img-mobile">
+      <img src="../assets/images/cyber-division-logo-full-2.png" class="logo" />
+    </div>
+    <footer class="footers-main footer-desktop">
+      <div class="footers-img img-desktop">
         <img
           src="../assets/images/cyber-division-logo-full-2.png"
           class="logo"
@@ -26,7 +29,32 @@
           <li class="footers-li">Susah, Imm Ibrahim, 6th Floor</li>
         </ul>
       </div>
-      <div class="footers-col">
+      <div class="footers-col footers-social">
+        <div class="footers-section">Follow us On Social Media</div>
+      </div>
+    </footer>
+    <footer class="footers-main footer-mobile">
+      <div class="footer-desc">
+        <div class="footers-col">
+          <div class="footers-section">Information</div>
+          <ul>
+            <li class="footers-li">About Us</li>
+            <li class="footers-li">Origins</li>
+            <li class="footers-li">FAQs</li>
+            <li class="footers-li">Contact</li>
+            <li class="footers-li">Legal</li>
+          </ul>
+        </div>
+        <div class="footers-col">
+          <div class="footers-section">Contact Us</div>
+          <ul>
+            <li class="footers-li">contact@cyberdivisino.tn</li>
+            <li class="footers-li">+216 29 065 054</li>
+            <li class="footers-li">Susah, Imm Ibrahim, 6th Floor</li>
+          </ul>
+        </div>
+      </div>
+      <div class="footers-col footers-social">
         <div class="footers-section">Follow us On Social Media</div>
       </div>
     </footer>
@@ -42,23 +70,56 @@ export default {};
   .divider {
     width: 100%;
     height: 1px;
-    flex-shrink: 0;
     background: linear-gradient(90deg, #e3ffe6 0%, #e5e1d6 100%);
     opacity: 0.3;
   }
+  .img-mobile {
+    display: none;
+    @media screen and (max-width: 990px) {
+      display: flex;
+      width: 100%;
+      margin: 30px auto 0;
+      justify-content: center;
+    }
+  }
   .footers-main {
-    display: flex;
     max-width: 2024px;
     margin: 0 auto;
     padding: 54px 0 100px;
     justify-content: space-around;
+    &.footer-desktop {
+      display: flex;
+      @media screen and (max-width: 600px) {
+        display: none;
+      }
+    }
+    &.footer-mobile {
+      display: none;
+      @media screen and (max-width: 600px) {
+        display: block;
+        margin: 0 auto;
+        max-width: 380px;
+        .footer-desc {
+          display: flex;
+          justify-content: space-between;
+        }
+        .footers-social {
+          margin-top: 41px;
+        }
+      }
+    }
+    .img-desktop {
+      display: block;
+      @media screen and (max-width: 990px) {
+        display: none;
+      }
+    }
     .logo {
       width: 159px;
       height: auto;
       object-fit: cover;
     }
     .footers-col {
-      display: block;
       .footers-section {
         color: #ecf9e8;
         font-family: "Aeonik Medium";
--- components/OurTeams.vue
+++ components/OurTeams.vue
@@ -1,33 +1,35 @@
 <template>
   <div id="OurTeam">
-    <div class="team-header">
-      Our teams are continuously developing innovative strategies, building new
-      technologies, and conducting ongoing research.
-    </div>
-    <div class="team-function">
-      <div
-        v-for="t in teams"
-        :key="t.name"
-        class="team-image"
-        :class="{ computer: t.name === 'Development' }"
-      >
-        <img
-          :src="imageSrc(t.team_img)"
-          class="function-img"
-          :class="{ computer: t.name === 'Development' }"
-        />
+    <div class="our-team">
+      <div class="team-header">
+        Our teams are continuously developing innovative strategies, building
+        new technologies, and conducting ongoing research.
+      </div>
+      <div class="team-function">
         <div
-          class="function-name"
+          v-for="t in teams"
+          :key="t.name"
+          class="team-image"
           :class="{ computer: t.name === 'Development' }"
         >
-          {{ t.name }}
+          <img
+            :src="imageSrc(t.team_img)"
+            class="function-img"
+            :class="{ computer: t.name === 'Development' }"
+          />
+          <div
+            class="function-name"
+            :class="{ computer: t.name === 'Development' }"
+          >
+            {{ t.name }}
+          </div>
         </div>
       </div>
+      <div class="team-header-2">
+        You’ve got questions, we’ve got answers... Let’s Chat!
+      </div>
+      <div class="btnTeam">Contact the Team</div>
     </div>
-    <div class="team-header-2">
-      You’ve got questions, we’ve got answers... Let’s Chat!
-    </div>
-    <div class="btnTeam">Contact the Team</div>
   </div>
 </template>
 
@@ -59,76 +61,159 @@ const imageSrc = (path) => {
 <style lang="scss" scoped>
 #OurTeam {
   padding-bottom: 250px;
-  .team-header {
-    margin-bottom: 40px;
-    padding-left: 24px;
-    color: #ecf9e8;
-    font-family: "Aeonik Regular";
-    font-size: 68.095px;
-    font-style: normal;
-    font-weight: 400;
-    line-height: 74.905px;
-  }
-  .team-function {
-    display: flex;
+  @media screen and (max-width: 990px) {
     width: 100%;
-    padding-bottom: 250px;
-    margin: 0 auto;
-    column-gap: 5px;
-    .team-image {
-      position: relative;
-      max-width: 20%;
-      max-height: 624px;
-      &.computer {
-        max-width: 40%;
+    padding-bottom: 100px;
+    margin: 0;
+  }
+  .our-team {
+    @media screen and (max-width: 990px) {
+      display: block;
+    }
+    .team-header {
+      margin-bottom: 40px;
+      padding-left: 24px;
+      color: #ecf9e8;
+      font-family: "Aeonik Regular";
+      font-size: 68.095px;
+      font-style: normal;
+      font-weight: 400;
+      line-height: 74.905px;
+      @media screen and (max-width: 1855px) {
+        font-size: 60px;
+        line-height: 70px;
       }
-      .function-name {
-        position: absolute;
-        bottom: 21px;
-        left: 19px;
-        color: #ecf9e8;
-        font-family: "Aeonik Bold";
-        font-size: 24px;
-        font-style: normal;
-        line-height: 36px;
-        &.computer {
-          left: 22px;
+      @media screen and (max-width: 1710px) {
+        font-size: 58px;
+        line-height: 64px;
+      }
+      @media screen and (max-width: 1597px) {
+        font-size: 50px;
+        line-height: 60px;
+      }
+      @media screen and (max-width: 1463px) {
+        font-size: 48px;
+        line-height: 54px;
+      }
+      @media screen and (max-width: 1430px) {
+        font-size: 40px;
+        line-height: 50px;
+      }
+      @media screen and (max-width: 990px) {
+        max-width: 380px;
+        margin: 0 auto 50px;
+        font-size: 30px;
+        line-height: 33px;
+        padding: 0;
+        text-align: justify;
+      }
+    }
+    .team-function {
+      display: flex;
+      width: 100%;
+      padding-bottom: 250px;
+      margin: 0 auto;
+      column-gap: 5px;
+      @media screen and (max-width: 990px) {
+        display: block;
+        padding-bottom: 100px;
+      }
+      .team-image {
+        position: relative;
+        max-width: 20%;
+        max-height: 624px;
+        @media screen and (max-width: 990px) {
+          max-width: 100%;
+          max-height: 150px;
+          margin-bottom: 5px;
         }
-      }
-      .function-img {
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
         &.computer {
-          transform: scaleX(-1);
+          max-width: 40%;
+          @media screen and (max-width: 990px) {
+            max-width: 100%;
+            max-height: 314px;
+          }
+        }
+        .function-name {
+          position: absolute;
+          bottom: 21px;
+          left: 19px;
+          color: #ecf9e8;
+          font-family: "Aeonik Bold";
+          font-size: 24px;
+          font-style: normal;
+          line-height: 36px;
+          &.computer {
+            left: 22px;
+          }
+        }
+        .function-img {
+          width: 100%;
+          height: 100%;
+          object-fit: cover;
+          @media screen and (max-width: 990px) {
+            max-height: 150px;
+          }
+          &.computer {
+            transform: scaleX(-1);
+            @media screen and (max-width: 990px) {
+              max-height: 314px;
+            }
+          }
         }
       }
     }
-  }
-  .team-header-2 {
-    margin: 0 auto 62px;
-    text-align: center;
-    color: #ecf9e8;
-    font-family: "Aeonik Regular";
-    font-size: 68.095px;
-    font-style: normal;
-    font-weight: 400;
-    line-height: 74.905px;
-  }
-  .btnTeam {
-    width: 180px;
-    height: 50px;
-    margin: 0 auto;
-    padding: 12px 0;
-    border-radius: 5px;
-    background: linear-gradient(90deg, #e3ffe6 0%, #e5e1d6 100%);
-    color: #1e3f04;
-    font-family: "Aeonik Medium";
-    font-size: 16px;
-    font-style: normal;
-    line-height: 24px;
-    letter-spacing: 0.48px;
-    text-align: center;
+    .team-header-2 {
+      margin: 0 auto 62px;
+      text-align: center;
+      color: #ecf9e8;
+      font-family: "Aeonik Regular";
+      font-size: 68.095px;
+      font-style: normal;
+      font-weight: 400;
+      line-height: 74.905px;
+      @media screen and (max-width: 1855px) {
+        font-size: 60px;
+        line-height: 70px;
+      }
+      @media screen and (max-width: 1710px) {
+        font-size: 58px;
+        line-height: 64px;
+      }
+      @media screen and (max-width: 1597px) {
+        font-size: 50px;
+        line-height: 60px;
+      }
+      @media screen and (max-width: 1463px) {
+        font-size: 48px;
+        line-height: 54px;
+      }
+      @media screen and (max-width: 1430px) {
+        font-size: 40px;
+        line-height: 50px;
+      }
+      @media screen and (max-width: 990px) {
+        font-size: 30px;
+        line-height: 33px;
+        padding: 0;
+        margin-bottom: 37px;
+      }
+    }
+    .btnTeam {
+      width: 180px;
+      height: 50px;
+      margin: 0 auto;
+      padding: 12px 0;
+      border-radius: 5px;
+      background: linear-gradient(90deg, #e3ffe6 0%, #e5e1d6 100%);
+      color: #1e3f04;
+      font-family: "Aeonik Medium";
+      font-size: 16px;
+      font-style: normal;
+      line-height: 24px;
+      letter-spacing: 0.48px;
+      text-align: center;
+    }
   }
 }
 </style>
